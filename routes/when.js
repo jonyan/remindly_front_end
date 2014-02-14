9481cfa8999fab5cf554cf2c48c6f1783b30c7ab
Jonathan Yan <j.yan93@gmail.com>
2014-02-14 05:45:53 +0000

    added buttons animation for nav-footer accross application

--- routes/when.js
+++ routes/when.js
@@ -2,6 +2,6 @@
 // get all of the recipients of the message and sed these numbers to backend then 
 // render when page
 
-exports.addRecipients = function(req, res) {    
+exports.addRecipients = function(req, res) {   	 
 	res.render('when');
  };
