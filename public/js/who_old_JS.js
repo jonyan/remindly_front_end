83a1e14a18c7c4c7105274039fd492b8d6d3cbb0
Jonathan Yan <jyan93@stanford.edu>
2014-02-27 22:32:19 -0800

    implemented old who page design

--- public/js/who_old_JS.js
+++ public/js/who_old_JS.js
@@ -8,3 +8,48 @@ $(function() {
 function submitWhoData() {
 	$('#submitBtn').click();
 }
+
+/* WHO PAGE */
+
+/* If user inputs name/number in the text field on 
+ * who page, this code will create another input field
+ * dynamically; up to 4 text fields if the 'me' checkbox
+ * is checked, up to 5 text fields if the 'me' checkbox
+ * is unchecked
+ */
+
+var numTextFields = 3;
+var recipientNumber = 2;
+
+$('#me').change(function() {
+	if ($('#me').is(':checked'))  {
+		numTextFields--;
+	} else {
+		numTextFields++;
+	}
+	if ((!$('#me').is(':checked')) && numTextFields <= 1) {
+		console.log("uncheck and add field");
+		addTextField();
+	}
+	if ($('#me').is(':checked') && numTextFields < 0) {
+		$("#recipient5").remove();
+		numTextFields++;
+		recipientNumber--;
+	}
+});
+
+
+function addTextField() {
+	if (numTextFields > 0) {
+		var newTextField = "<tr id='recipient" + recipientNumber
+			+ "'><td class='contact_row'><input class='recipient_textbox' id='recipient"
+			+ recipientNumber + "' type='tel' onclick='addTextField()' placeholder='Add "
+			+ numTextFields + " more phone #s or names...' name='recipient"
+			+ recipientNumber +"'></td></tr>";
+		$(newTextField).insertAfter('#recipient' + (recipientNumber - 1));
+		numTextFields--;
+		recipientNumber++;
+	}
+	console.log(numTextFields);
+
+}
