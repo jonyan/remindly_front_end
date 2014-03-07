bd06e3ec94ad15a2dba664213682b2aeacf0a775
Jonathan Yan <jyan93@stanford.edu>
2014-03-06 23:38:03 -0800

    finishing touches

--- public/js/edit_contact_JS.js
+++ public/js/edit_contact_JS.js
@@ -31,16 +31,23 @@ function updateContactsData() {
 	var user_id = $.cookie('user_id');
 	var name = $("#contact_name").val();
 	var phone = $("#contact_phone").val();
+	
+	if (isEmpty()) {
+		alert("Field left empty!");
+	} else if (isInvalid(phone)) {
+		alert("Please enter a valid phone number!")
+	} else {
 
+		$.post("http://www.aerodroid.com/remindly/edit_contact.php",
+			{
+				"contact_id" : contact_id,
+				"user_id" : user_id,
+				"name" : name, 			// new name
+				"phone" : phone		// new phone
+			},
+		onFinishEdit);
+	}
 
-	$.post("http://www.aerodroid.com/remindly/edit_contact.php",
-		{
-			"contact_id" : contact_id,
-			"user_id" : user_id,
-			"name" : name, 			// new name
-			"phone" : phone		// new phone
-		},
-	onFinishEdit);
 }
 
 function onFinishEdit() {
@@ -48,6 +55,23 @@ function onFinishEdit() {
 	window.location.href = "/contacts";
 }
 
+
+function isInvalid(phone) {
+	var nondigits = /\D/g;
+	console.log(phone);
+	if (phone.length != 10 || nondigits.test(phone)) {
+		console.log("returning true");
+		return true;
+	}
+	return false;
+}
+
+function isEmpty() {
+	if ($("#contact_name").val() == "" || $('#contact_phone').val() == "") {
+		return true;
+	}
+	return false;
+}
 
 $(document).on('click', '.edit_contact_delete_btn', function() {
 	var result = confirm("Are you sure?")
